9007f7ee7b153054fb167b9becb7f4928cd73e25
Josephine Han <jsphnhan@gmail.com>
2021-06-16 23:59:01 -0700

    config adjusted

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://pure-atoll-97949.herokuapp.com/`,
+  API_ENDPOINT: process.env.API_ENDPOINT,
   //API_ENDPOINT: process.env.API_ENDPOINT,
   TOKEN_KEY: "glampers-client-auth-token",
   API_KEY: process.env.API_KEY,
